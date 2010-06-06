a7f3402e0d0e6a52dbf74eeb5d8b84af2fac8e91
Wysie <sohyuanchin@gmail.com>
2010-06-06 18:33:56 +0800

    Export to XML is working. Import is not implemented yet.

--- src/com/android/launcher/MyLauncherSettings.java
+++ src/com/android/launcher/MyLauncherSettings.java
@@ -9,9 +9,11 @@ import android.app.PendingIntent;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
@@ -20,13 +22,33 @@ import android.preference.Preference.OnPreferenceChangeListener;
 import android.preference.Preference.OnPreferenceClickListener;
 import android.util.Log;
 
+import java.io.FileReader;
+import org.xmlpull.v1.XmlPullParser;
+import org.xmlpull.v1.XmlPullParserFactory;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.xmlpull.v1.XmlPullParserException;
+import android.graphics.Color;
+import android.widget.Toast;
+import java.io.FileWriter;
+import org.xmlpull.v1.XmlSerializer;
+import android.util.Xml;
+import java.util.ArrayList;
+import java.io.File;
+
 public class MyLauncherSettings extends PreferenceActivity implements OnPreferenceChangeListener {
+
     private static final String ALMOSTNEXUS_PREFERENCES = "launcher.preferences.almostnexus";
     private boolean shouldRestart=false;
     private static final String FROYOMSG="Changing this setting will make the Launcher restart itself";
-    private static final String NORMALMSG="Changing this setting will make the Launcher restart itself";
+    private static final String NORMALMSG="Changing this setting will make the Launcher restart itself";   
     private String mMsg;
     private Context mContext;
+    
+    private static final String XML_FILENAME = "adw_settings.xml";
+    
+    
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
 		//TODO: ADW should i read stored values after addPreferencesFromResource?
@@ -67,11 +89,61 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 				return true;
 			}
 		});
+		
         Preference uiHideLabels = (Preference) findPreference("uiHideLabels");
         uiHideLabels.setOnPreferenceChangeListener(this);
         Preference mDateColorPref = findPreference("highlights_color");
+        
         mContext=this;
+        
+        Preference importXML = findPreference("xml_import");
+        importXML.setOnPreferenceClickListener(new OnPreferenceClickListener() {
+			public boolean onPreferenceClick(Preference preference) {
+                AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+                alertDialog.setTitle(getResources().getString(R.string.title_dialog_xml));
+                alertDialog.setMessage(getResources().getString(R.string.message_dialog_import));
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(android.R.string.ok), 
+                    new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        importXML();
+                    }
+                });
+                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(android.R.string.cancel), 
+                    new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    
+                    }
+                });
+                alertDialog.show();
+                return true;
+            }
+        });        
+        
+        Preference exportXML = findPreference("xml_export");
+        exportXML.setOnPreferenceClickListener(new OnPreferenceClickListener() {        
+			public boolean onPreferenceClick(Preference preference) {
+                AlertDialog alertDialog = new AlertDialog.Builder(mContext).create();
+                alertDialog.setTitle(getResources().getString(R.string.title_dialog_xml));
+                alertDialog.setMessage(getResources().getString(R.string.message_dialog_export));
+                alertDialog.setButton(DialogInterface.BUTTON_POSITIVE, getResources().getString(android.R.string.ok), 
+                    new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                        exportXML();
+                    }
+                });
+                alertDialog.setButton(DialogInterface.BUTTON_NEGATIVE, getResources().getString(android.R.string.cancel), 
+                    new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int which) {
+                    
+                    }
+                });
+                alertDialog.show();
+                return true;
+            }
+        });        
+
     }
+    
 	@Override
 	protected void onPause(){
 		if(shouldRestart){
@@ -95,6 +167,7 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 		}
 		super.onPause();
 	}
+	
 	public boolean onPreferenceChange(Preference preference, Object newValue) {
 		if (preference.getKey().equals("desktopScreens")) {
 			dlgSeekBarPreference pref = (dlgSeekBarPreference) findPreference("defaultScreen");
@@ -102,7 +175,7 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage(mMsg)
 			       .setCancelable(false)
-			       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			       .setPositiveButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
 			           public void onClick(DialogInterface dialog, int id) {
 							shouldRestart=true;
 			           }
@@ -113,7 +186,7 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage(mMsg)
 			       .setCancelable(false)
-			       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			       .setPositiveButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
 			           public void onClick(DialogInterface dialog, int id) {
 							shouldRestart=true;
 			           }
@@ -124,7 +197,7 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage(mMsg)
 			       .setCancelable(false)
-			       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			       .setPositiveButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
 			           public void onClick(DialogInterface dialog, int id) {
 							shouldRestart=true;
 			           }
@@ -135,7 +208,7 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage(mMsg)
 			       .setCancelable(false)
-			       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			       .setPositiveButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
 			           public void onClick(DialogInterface dialog, int id) {
 							shouldRestart=true;
 			           }
@@ -146,7 +219,7 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 			AlertDialog.Builder builder = new AlertDialog.Builder(this);
 			builder.setMessage(mMsg)
 			       .setCancelable(false)
-			       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			       .setPositiveButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
 			           public void onClick(DialogInterface dialog, int id) {
 							shouldRestart=true;
 			           }
@@ -158,9 +231,13 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 	}    
 
     public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
-        if (preference.getKey().equals("highlights_color")) {
-        	ColorPickerDialog cp = new ColorPickerDialog(this,mHighlightsColorListener,readHighlightsColor());
-        	cp.show();
+        try {
+            if (preference.getKey().equals("highlights_color")) {
+            	ColorPickerDialog cp = new ColorPickerDialog(this,mHighlightsColorListener,readHighlightsColor());
+            	cp.show();
+            }
+        }
+        catch (NullPointerException e) {
         }
         return false;
 	}
@@ -175,7 +252,7 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 			AlertDialog.Builder builder = new AlertDialog.Builder(mContext);
 			builder.setMessage(mMsg)
 			       .setCancelable(false)
-			       .setPositiveButton("Ok", new DialogInterface.OnClickListener() {
+			       .setPositiveButton(getResources().getString(android.R.string.ok), new DialogInterface.OnClickListener() {
 			           public void onClick(DialogInterface dialog, int id) {
 							shouldRestart=true;
 			           }
@@ -184,5 +261,152 @@ public class MyLauncherSettings extends PreferenceActivity implements OnPreferen
 			alert.show();
     		getPreferenceManager().getSharedPreferences().edit().putInt("highlights_color", color).commit();
     	}
-};
+    };
+    
+    private void importXML() {        
+        shouldRestart=true;
+    }
+    
+    private void exportXML() {
+        if (!Environment.MEDIA_MOUNTED.equals(Environment.getExternalStorageState())) {
+            Toast.makeText(mContext, R.string.xml_sdcard_unmounted, Toast.LENGTH_SHORT).show();
+            return;
+        }        
+        
+        FileWriter writer = null;
+        File outFile = new File(Environment.getExternalStorageDirectory() + "/" + "tempADWSettings.xml");
+        boolean success = false;
+        
+        try {            
+            outFile.createNewFile();
+            writer = new FileWriter(outFile);
+            XmlSerializer serializer = Xml.newSerializer();
+            serializer.setOutput(writer);
+            
+            // Start XML
+            serializer.startDocument("UTF-8", true);
+            serializer.startTag("", "adw_settings");            
+            
+            // Screen Prefs
+            serializer.startTag("", "wallpaperHack");
+            serializer.text("" + AlmostNexusSettingsHelper.getWallpaperHack(this));
+            serializer.endTag("", "wallpaperHack");            
+            serializer.startTag("", "desktopRotation");
+            serializer.text("" + AlmostNexusSettingsHelper.getDesktopRotation(this));
+            serializer.endTag("", "desktopRotation");            
+            serializer.startTag("", "hideStatusbar");
+            serializer.text("" + AlmostNexusSettingsHelper.getHideStatusbar(this));
+            serializer.endTag("", "hideStatusbar");            
+            serializer.startTag("", "desktopScreens");
+            serializer.text("" + AlmostNexusSettingsHelper.getDesktopScreens(this));
+            serializer.endTag("", "desktopScreens");            
+            serializer.startTag("", "defaultScreen");
+            serializer.text("" + AlmostNexusSettingsHelper.getDefaultScreen(this));
+            serializer.endTag("", "defaultScreen");            
+            serializer.startTag("", "desktopSpeed");
+            serializer.text("" + AlmostNexusSettingsHelper.getDesktopSpeed(this));
+            serializer.endTag("", "desktopSpeed");            
+            serializer.startTag("", "desktopBounce");
+            serializer.text("" + AlmostNexusSettingsHelper.getDesktopBounce(this));
+            serializer.endTag("", "desktopBounce");
+            
+            // Drawer Settings
+            serializer.startTag("", "zoomSpeed");
+            serializer.text("" + AlmostNexusSettingsHelper.getZoomSpeed(this));
+            serializer.endTag("", "zoomSpeed");            
+            serializer.startTag("", "drawerNew");
+            serializer.text("" + AlmostNexusSettingsHelper.getDrawerNew(this));
+            serializer.endTag("", "drawerNew");            
+            serializer.startTag("", "drawerAnimated");
+            serializer.text("" + AlmostNexusSettingsHelper.getDrawerAnimated(this));
+            serializer.endTag("", "drawerAnimated");            
+            serializer.startTag("", "drawerAlpha");
+            serializer.text("" + AlmostNexusSettingsHelper.getDrawerAlpha(this));
+            serializer.endTag("", "drawerAlpha");            
+            serializer.startTag("", "drawerColumnsPortrait");
+            serializer.text("" + AlmostNexusSettingsHelper.getColumnsPortrait(this));
+            serializer.endTag("", "drawerColumnsPortrait");            
+            serializer.startTag("", "drawerRowsPortrait");
+            serializer.text("" + AlmostNexusSettingsHelper.getRowsPortrait(this));
+            serializer.endTag("", "drawerRowsPortrait");            
+            serializer.startTag("", "drawerColumnsLandscape");
+            serializer.text("" + AlmostNexusSettingsHelper.getColumnsLandscape(this));
+            serializer.endTag("", "drawerColumnsLandscape");            
+            serializer.startTag("", "drawerRowsLandscape");
+            serializer.text("" + AlmostNexusSettingsHelper.getRowsLandscape(this));
+            serializer.endTag("", "drawerRowsLandscape");
+            
+            // Preview Settings
+            serializer.startTag("", "previewsNew");
+            serializer.text("" + AlmostNexusSettingsHelper.getNewPreviews(this));
+            serializer.endTag("", "previewsNew");            
+            serializer.startTag("", "previewsFullScreen");
+            serializer.text("" + AlmostNexusSettingsHelper.getFullScreenPreviews(this));
+            serializer.endTag("", "previewsFullScreen");
+            
+            // UI Settings
+            serializer.startTag("", "homeBinding");
+            serializer.text("" + AlmostNexusSettingsHelper.getHomeBinding(this));
+            serializer.endTag("", "homeBinding");            
+            serializer.startTag("", "uiDots");
+            serializer.text("" + AlmostNexusSettingsHelper.getUIDots(this));
+            serializer.endTag("", "uiDots");            
+            serializer.startTag("", "uiDockbar");
+            serializer.text("" + AlmostNexusSettingsHelper.getUIDockbar(this));
+            serializer.endTag("", "uiDockbar");            
+            serializer.startTag("", "uiCloseDockbar");
+            serializer.text("" + AlmostNexusSettingsHelper.getUICloseDockbar(this));
+            serializer.endTag("", "uiCloseDockbar");            
+            serializer.startTag("", "uiLAB");
+            serializer.text("" + AlmostNexusSettingsHelper.getUILAB(this));
+            serializer.endTag("", "uiLAB");            
+            serializer.startTag("", "uiRAB");
+            serializer.text("" + AlmostNexusSettingsHelper.getUIRAB(this));
+            serializer.endTag("", "uiRAB");            
+            serializer.startTag("", "uiTint");
+            serializer.text("" + AlmostNexusSettingsHelper.getUITint(this));
+            serializer.endTag("", "uiTint");            
+            serializer.startTag("", "uiScaleAB");
+            serializer.text("" + AlmostNexusSettingsHelper.getuiScaleAB(this));
+            serializer.endTag("", "uiScaleAB");            
+            serializer.startTag("", "uiAppsBg");
+            serializer.text("" + AlmostNexusSettingsHelper.getUIAppsBg(this));
+            serializer.endTag("", "uiAppsBg");            
+            serializer.startTag("", "uiABBg");
+            serializer.text("" + AlmostNexusSettingsHelper.getUIABBg(this));
+            serializer.endTag("", "uiABBg");            
+            serializer.startTag("", "uiHideLabels");
+            serializer.text("" + AlmostNexusSettingsHelper.getUIHideLabels(this));
+            serializer.endTag("", "uiHideLabels");            
+            serializer.startTag("", "highlights_color");
+            serializer.text("" + AlmostNexusSettingsHelper.getHighlightsColor(this));
+            serializer.endTag("", "highlights_color");
+            
+            // End XML
+            serializer.endTag("", "adw_settings");
+            serializer.endDocument();
+            serializer.flush();
+            success = true;
+        }
+        catch (Exception e) {
+            Toast.makeText(mContext, R.string.xml_write_error, Toast.LENGTH_SHORT).show();
+        }
+        finally {
+            if (writer != null) {
+        		try {
+	        	    writer.close();
+	        	} catch (IOException e) {
+	        	}
+	        }
+        }
+        
+        if (success) {
+            File xmlFile = new File(Environment.getExternalStorageDirectory() + "/" + XML_FILENAME);
+            outFile.renameTo(xmlFile);
+            Toast.makeText(mContext, R.string.xml_export_success, Toast.LENGTH_SHORT).show();
+        }
+        
+        if (outFile.exists())
+            outFile.delete();
+    }
 }
